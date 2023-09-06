0dbb35125e13f00b6f3a9203f3d2592827510055
lucas-hirata <lucashirata.dev@gmail.com>
2023-09-06 16:25:53 -0300

    Implemented HrDatabaseContext

---
+++ Hr.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -0,0 +1,41 @@
+﻿using Hr.LeaveManagement.Domain;
+using Hr.LeaveManagement.Domain.Common;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Hr.LeaveManagement.Persistence.DatabaseContext;
+
+public class HrDatabaseContext : DbContext
+{
+	public HrDatabaseContext(DbContextOptions<HrDatabaseContext> options) : base(options)
+	{
+
+	}
+
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        {
+            entry.Entity.ModifiedAt = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
