f5ee36f5b3531b618b4bf2bb229c14dfb75bd9a5
lucas-hirata <lucashirata.dev@gmail.com>
2023-09-06 13:01:50 -0300

    Implemented GetLeaveTypeDetails feature

---
+++ Hr.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Hr.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace Hr.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public GetLeaveTypeDetailsQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        this._mapper = mapper;
+        this._leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var leaveTypeDetails = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+        var result = _mapper.Map<LeaveTypeDetailsDto>(leaveTypeDetails);
+
+        return result;
+    }
+}
