32721497bb388e8913488310e034b4ef2d4fd2c9
lucas-hirata <lucashirata.dev@gmail.com>
2023-09-06 16:26:46 -0300

    Implemented LeaveTypeConfiguration for persistence

---
+++ Hr.LeaveManagement.Persistence/Configurations/LeaveTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Hr.LeaveManagement.Domain;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hr.LeaveManagement.Persistence.Configurations;
+
+public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+{
+    public void Configure(EntityTypeBuilder<LeaveType> builder)
+    {
+        builder.HasData(
+            new LeaveType
+            {
+                Id = 1,
+                Name = "Vacation",
+                DefaultDays = 10,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now
+            }
+        );
+
+        builder.Property(q => q.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
