e78d157991c74ee41b54c1a788a4612598f64e9a
lucas-hirata <lucashirata.dev@gmail.com>
2023-09-06 16:26:24 -0300

    Implemented service registration for Persistence project

---
+++ Hr.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Hr.LeaveManagement.Application.Contracts.Persistence;
+using Hr.LeaveManagement.Persistence.DatabaseContext;
+using Hr.LeaveManagement.Persistence.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hr.LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<HrDatabaseContext>(options => {
+            options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+        });
+
+        return services;
+    }
+}
