2d2657facb5ab85ea4344f1f0081d03d8aa6d043
lucas-hirata <lucashirata.dev@gmail.com>
2023-09-11 17:20:27 -0300

    Implemented update leave type validation

--- Hr.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
+++ Hr.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -1,5 +1,10 @@
 ﻿using AutoMapper;
+
+using Hr.LeaveManagement.Application.Contracts.Logging;
 using Hr.LeaveManagement.Application.Contracts.Persistence;
+using Hr.LeaveManagement.Application.Exceptions;
+using Hr.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
 using MediatR;
 
 namespace Hr.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
@@ -8,21 +13,34 @@ public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeComm
 {
     private readonly IMapper _mapper;
     private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IApiLogger<UpdateLeaveTypeCommandHandler> _logger;
 
-    public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    public UpdateLeaveTypeCommandHandler(
+        IMapper mapper,
+        ILeaveTypeRepository leaveTypeRepository,
+        IApiLogger<UpdateLeaveTypeCommandHandler> logger)
     {
         this._mapper = mapper;
         this._leaveTypeRepository = leaveTypeRepository;
+        this._logger = logger;
     }
     public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
     {
-        // TODO: Validate incoming data
+        // Validate incoming data
+        var validator = new UpdateLeaveTypeCommandValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (!validationResult.IsValid)
+        {
+            _logger.LogWarning("Validation errors in update request for {0} - {1}", nameof(LeaveType), request.Id);
+            throw new BadRequestException("Invalid leave type", validationResult);
+        }
 
         // Convert to domain entity object
-        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+        var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
 
         // Add to database
-        await _leaveTypeRepository.UpdateAsync(leaveTypeToCreate);
+        await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
 
         // Return Unit value
         return Unit.Value;
---
+++ Hr.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+using Hr.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+
+namespace Hr.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class UpdateLeaveTypeCommandValidator : AbstractValidator<UpdateLeaveTypeCommand>
+{
+    public UpdateLeaveTypeCommandValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .NotNull()
+            .MaximumLength(70).WithMessage("{PropertyName must be fewer than 70 characters}");
+
+        RuleFor(p => p.DefaultDays)
+            .GreaterThan(1).WithMessage("{PropertyName} cannot be less than 1")
+            .LessThan(100).WithMessage("{PropertyName} cannot exceed 100");
+    }
+}
