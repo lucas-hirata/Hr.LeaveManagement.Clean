bba52333d000d79baceca68a9927bd98919cd331
lucas-hirata <lucashirata.dev@gmail.com>
2023-09-11 17:22:43 -0300

    Added logging to get all leave types handler

--- Hr.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetAllLeaveTypesQueryHandler.cs
+++ Hr.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetAllLeaveTypesQueryHandler.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+
+using Hr.LeaveManagement.Application.Contracts.Logging;
 using Hr.LeaveManagement.Application.Contracts.Persistence;
 using MediatR;
 
@@ -8,11 +10,16 @@ public class GetAllLeaveTypesQueryHandler : IRequestHandler<GetAllLeaveTypesQuer
 {
     private readonly IMapper _mapper;
     private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IApiLogger<GetAllLeaveTypesQueryHandler> _logger;
 
-    public GetAllLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    public GetAllLeaveTypesQueryHandler(
+        IMapper mapper,
+        ILeaveTypeRepository leaveTypeRepository,
+        IApiLogger<GetAllLeaveTypesQueryHandler> logger)
     {
         this._mapper = mapper;
         this._leaveTypeRepository = leaveTypeRepository;
+        this._logger = logger;
     }
 
     public async Task<IEnumerable<LeaveTypeDto>> Handle(GetAllLeaveTypesQuery request, CancellationToken cancellationToken)
@@ -24,6 +31,7 @@ public class GetAllLeaveTypesQueryHandler : IRequestHandler<GetAllLeaveTypesQuer
         var data = _mapper.Map<IEnumerable<LeaveTypeDto>>(leaveTypes);
 
         // return list of dtos
+        _logger.LogInformation("Leave types were retrieved successfully");
         return data;
     }
 }
