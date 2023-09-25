2afd87f8d596fb737e984c95e33b2760c47f1f7c
lucas-hirata <lucashirata.dev@gmail.com>
2023-09-25 13:52:42 -0300

    Added mapping profiles to latest added features,

---
+++ Hr.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+using Hr.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+using Hr.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation;
+using Hr.LeaveManagement.Application.Features.LeaveAllocation.Commands.UpdateLeaveAllocation;
+using Hr.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetAllLeaveAllocations;
+using Hr.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using Hr.LeaveManagement.Domain;
+
+namespace Hr.LeaveManagement.Application.MappingProfiles;
+
+public class LeaveAllocationProfile : Profile
+{
+    public LeaveAllocationProfile()
+    {
+        CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+        CreateMap<LeaveAllocation, LeaveAllocationDetailsDto>();
+        CreateMap<CreateLeaveAllocationCommand, LeaveAllocation>();
+        CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
+        CreateMap<DeleteLeaveAllocationCommand, LeaveAllocation>();
+    }
+}
