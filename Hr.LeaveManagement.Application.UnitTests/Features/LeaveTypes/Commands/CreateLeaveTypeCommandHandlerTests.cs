9e5ae770f2729e266cd15ce3c474fe732eebb0a2
lucas-hirata <lucashirata.dev@gmail.com>
2024-01-09 14:25:29 -0300

    Started unit test implementation for application layer

---
+++ Hr.LeaveManagement.Application.UnitTests/Features/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+
+using Hr.LeaveManagement.Application.Contracts.Logging;
+using Hr.LeaveManagement.Application.Contracts.Persistence;
+using Hr.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using Hr.LeaveManagement.Application.MappingProfiles;
+using Hr.LeaveManagement.Application.UnitTests.Mocks;
+
+using Moq;
+
+using Shouldly;
+
+namespace Hr.LeaveManagement.Application.UnitTests.Features.LeaveTypes.Commands;
+
+public class CreateLeaveTypeCommandHandlerTests
+{
+    private readonly Mock<ILeaveTypeRepository> _leaveTypeRepositoryMock;
+    private IMapper _mapper;
+    private Mock<IApiLogger<CreateLeaveTypeCommandHandler>> _mockApiLogger;
+
+    public CreateLeaveTypeCommandHandlerTests()
+    {
+        var mapperConfig = new MapperConfiguration(c => c.AddProfile<LeaveTypeProfile>());
+        _mapper = mapperConfig.CreateMapper();
+        _leaveTypeRepositoryMock = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+        _mockApiLogger = new Mock<IApiLogger<CreateLeaveTypeCommandHandler>>();
+    }
+
+    [Fact]
+    public async Task CreateLeaveTypeTest()
+    {
+        CreateLeaveTypeCommandHandler handler = new(_mapper, _leaveTypeRepositoryMock.Object);
+
+        var result = await handler.Handle(new CreateLeaveTypeCommand { Name = "Test Create", DefaultDays = 5 }, CancellationToken.None);
+
+        result.ShouldBe(4);
+
+        var data = await _leaveTypeRepositoryMock.Object.GetByIdAsync(4);
+        data.ShouldNotBeNull();
+    }
+}
---
+++ Hr.LeaveManagement.Application.UnitTests/Features/LeaveTypes/Queries/GetAllLeaveTypesQueryHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+
+using Hr.LeaveManagement.Application.Contracts.Logging;
+using Hr.LeaveManagement.Application.Contracts.Persistence;
+using Hr.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using Hr.LeaveManagement.Application.MappingProfiles;
+using Hr.LeaveManagement.Application.UnitTests.Mocks;
+
+using Moq;
+
+using Shouldly;
+
+namespace Hr.LeaveManagement.Application.UnitTests.Features.LeaveTypes.Queries;
+
+public class GetAllLeaveTypesQueryHandlerTests
+{
+    private readonly Mock<ILeaveTypeRepository> _leaveTypeRepositoryMock;
+    private IMapper _mapper;
+    private Mock<IApiLogger<GetAllLeaveTypesQueryHandler>> _mockApiLogger;
+
+    public GetAllLeaveTypesQueryHandlerTests()
+    {
+        _leaveTypeRepositoryMock = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+        var mapperConfig = new MapperConfiguration(c => c.AddProfile<LeaveTypeProfile>());
+        _mapper = mapperConfig.CreateMapper();
+        _mockApiLogger = new Mock<IApiLogger<GetAllLeaveTypesQueryHandler>>();
+    }
+
+    [Fact]
+    public async Task GetAllLeaveTypesTest()
+    {
+        GetAllLeaveTypesQueryHandler handler = new (
+            _mapper,
+            _leaveTypeRepositoryMock.Object,
+            _mockApiLogger.Object);
+
+        var result = await handler.Handle(new GetAllLeaveTypesQuery(), CancellationToken.None);
+
+        result.ShouldBeOfType<List<LeaveTypeDto>>();
+        result.Count().ShouldBe(3);
+    }
+}
