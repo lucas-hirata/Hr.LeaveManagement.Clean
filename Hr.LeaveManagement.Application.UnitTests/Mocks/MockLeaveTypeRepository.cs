03f6a556456756253676c670d5fb901a0e2f3015
lucas-hirata <lucashirata.dev@gmail.com>
2024-02-15 10:45:56 -0300

    Created Tests and UI Project

--- Hr.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
+++ Hr.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -11,20 +11,27 @@ public class MockLeaveTypeRepository
     {
         var leaveTypes = new List<LeaveType>
         {
-            new LeaveType { Id = 1, DefaultDays = 10, Name = "Test Vacation"},
-            new LeaveType { Id = 2, DefaultDays = 15, Name = "Test Sick"},
-            new LeaveType { Id = 3, DefaultDays = 15, Name = "Test Maternity"}
+            new() { Id = 1, DefaultDays = 10, Name = "Test Vacation"},
+            new() { Id = 2, DefaultDays = 15, Name = "Test Sick"},
+            new() { Id = 3, DefaultDays = 15, Name = "Test Maternity"}
         };
 
         var mockRepo = new Mock<ILeaveTypeRepository>();
         mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
 
+        mockRepo.Setup(r => r.GetByIdAsync(It.IsAny<int>()))
+            .ReturnsAsync((int id) => leaveTypes.FirstOrDefault(l => l.Id == id));
+
         mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
             .Returns((LeaveType leaveType) =>
             {
+                leaveType.Id = leaveTypes.Count + 1;
                 leaveTypes.Add(leaveType);
                 return Task.CompletedTask;
             });
+
+        mockRepo.Setup(r => r.IsLeaveTypeUnique(It.IsAny<string>()))
+            .ReturnsAsync((string name) => !leaveTypes.Any(l => l.Name == name));
 
         return mockRepo;
     }
---
+++ Hr.LeaveManagement.Persistence.IntegrationTests/HrDatabaseContextTests.cs
@@ -0,0 +1,59 @@
+﻿using Hr.LeaveManagement.Domain;
+using Hr.LeaveManagement.Persistence.DatabaseContext;
+
+using Microsoft.EntityFrameworkCore;
+
+using Shouldly;
+
+namespace Hr.LeaveManagement.Persistence.IntegrationTests;
+
+public class HrDatabaseContextTests
+{
+    private readonly HrDatabaseContext _hrDatabaseContext;
+
+    public HrDatabaseContextTests()
+	{
+		var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
+			.UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+		_hrDatabaseContext = new HrDatabaseContext(dbOptions);
+	}
+
+	[Fact]
+	public async Task Save_SetDateCreatedValue()
+	{
+		// Arrange
+		var leaveType = new LeaveType
+		{
+			Id = 1,
+			DefaultDays = 1,
+			Name = "Test",
+		};
+
+		// Act
+		await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+        await _hrDatabaseContext.SaveChangesAsync();
+
+		// Assert
+		leaveType.CreatedAt.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async Task Save_SetDateModifiedValue()
+    {
+        // Arrange
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 1,
+            Name = "Test",
+        };
+
+        // Act
+        await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+        await _hrDatabaseContext.SaveChangesAsync();
+
+        // Assert
+        leaveType.ModifiedAt.ShouldNotBeNull();
+    }
+}
